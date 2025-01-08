0c5d84e18c1c0d0b21e923d29fd5d76187bf71c7
H82M19\Student <Student>
2025-01-08 10:34:31 +0700

    Finish JF Part 1 Challenge 3

--- JF_P1_C3.js
+++ JF_P1_C3.js
@@ -1,4 +1,26 @@
 var scoreDolphins = [96, 108, 89];
 var scoreKoalas = [88, 91, 110];
 
-var avgScore = 
+var avgScore = (scores) => {
+    return scores.reduce((sum, score) => sum + score ,0) / scores.length;
+}
+
+var avgScoreDolphin = avgScore(scoreDolphins);
+var avgScoreKoala = avgScore(scoreKoalas);
+
+if(avgScoreDolphin > avgScoreKoala){
+    if(avgScoreDolphin >= 100)
+        console.log("Dolphins win!");
+    else console.log("Dolphins is higher but not at least 100 points");
+}
+else if(avgScoreDolphin < avgScoreKoala){
+    if(avgScoreKoala >= 100)
+    console.log("Koalas win!");
+else console.log("Koalas is higher but not at least 100 points");
+}
+else {
+    if(avgScoreDolphin >= 100)
+        console.log("Draw");
+    else console.log("No team wins");
+}
+
